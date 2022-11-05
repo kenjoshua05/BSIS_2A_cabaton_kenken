026ea3079a1dc0abc2da949e53a028020c7b9fe4
kenjoshua05 <112688989+kenjoshua05@users.noreply.github.com>
2022-11-05 15:39:22 +0800

    Add files via upload

---
+++ onlinefood-order/admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">© <?php echo date("Y"); ?> Tasty Buds Cuisine. All Rights Reserved</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
